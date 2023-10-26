63bb8cba2d82c2df3d3cdfd3e0a9477f121e9da9
unknown <tranthitrinh4013@gmail.com>
2023-10-26 15:10:22 +0700

    add home page from main

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,9 @@
 const images = {
-<<<<<<< HEAD
-  logoUAH: require("./logoUAH.png"),
-  bgHome: require("./bg.png"),
   bgData: require("./bg_data.jpg"),
   bgData2: require("./bg_data2.jpg"),
   bgData3: require("./bg_data3.jpg"),
-=======
 	logoUAH: require('./images/logoUAH.png'),
 	bgHome: require('./images/backgrHome.jpg'),
->>>>>>> 44d3a7980b00c29acb357c8e5d1e18d7fc32a8f1
 };
 const icons = {};
 
