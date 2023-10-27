af6cef569750e3c25fbf8c8995751a1cb56b3851
unknown <tranthitrinh4013@gmail.com>
2023-10-27 13:12:29 +0700

    update color, charts, banner home

--- src/pages/InvoiceDashboard.js
+++ src/pages/InvoiceDashboard.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import 'w3-css/w3.css';
+import React, { useState, useRef } from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import "w3-css/w3.css";
 import styled from "styled-components";
 import { images } from "../assets";
-const InvoiceDashboardStyle = styled.div``;
-const BannerImgID= styled.div`
+
+const ConsumeDashboardStyle = styled.div``;
+const BannerImgID = styled.div`
   position: relative;
   height: 7.5rem;
   width: 100%;
@@ -31,12 +32,8 @@ const BannerImgID= styled.div`
     padding: 0 10rem;
     margin: 0;
   }
-`;
-const BodyStylesID = styled.div`
-  width: 100%;
-`;
-const TextStyleID = styled.div`
-  h3 {
+}`;
+const TitleStyled = styled.h3`
     margin: 0;
     width: 30%;
     padding: 2rem 0;
@@ -45,161 +42,148 @@ const TextStyleID = styled.div`
     font-style: normal;
     font-weight: 700;
     line-height: 1.56;
-  }
-  span {
-    display: flex;
-    vertical-align: middle;
-    width: 100%;
-    padding: 2rem 0;
-  }
-  div{
-    width: 33.3333333333%;
-    p {
-        font-size: 1.1rem;
-        font-weight: 700;
-        color: #434343;
-    }
-    input[type="date"]::-webkit-calendar-picker-indicator {
-      color: #2c3e50;
-    }
-    input[type="date"] {
-        border-radius: 3px;
-        -webkit-appearance: none;
-        color: #95a5a6;
-        font-family: "Helvetica", arial, sans-serif;
-        font-size: 18px;
-        border:1px solid #a4acb4;
-        background:#ecf0f1;
-        padding:5px;
-        display: inline-block !important;
-        visibility: visible !important;
-    }
-    
-    input[type="date"], focus {
-        color: #393939;
-        box-shadow: none;
-        -webkit-box-shadow: none;
-        -moz-box-shadow: none;
-    }
-  }
-  form {
-    padding:3rem 0;
-    width: 41.6666666667%;   
-  }
-  content{
-    display: flex;
-    justify-content: space-between;
-    select{
-      width: 100%;
-      height: 100%;
-      margin-top: 0;
-      margin-bottom: 0;
-      max-width: 1280px;
-      padding: 14px 8px;
-    }
-  }
 `;
-
-const BorderStyleID = styled.div`
-  border-bottom: 1px solid rgb(192 192 192);
-`
 const ButtonStyle = styled(Button)`
-  width: 100%;
-  margin-top: 2.4rem;
-  margin-bottom: 4rem;
-  a {
-    margin: 0;
-    padding: 0.5rem 3rem;
     width: 100%;
-    text-decoration: none;
-  }
-`
+    margin-top: 2.4rem;
+    margin-bottom: 4rem;
+    a {
+        margin: 0;
+        color: #fff;
+        padding: 0.5rem 3rem;
+        width: 100%;
+        text-decoration: none;
+    }
+`;
 const FooterStyle = styled.div`
-  width: 100%;
-  font-size: 0.8rem;
-  font-weight: 400;
-  color: rgba(33, 33, 33, 1);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding-bottom: 1rem;
+    width: 100%;
+    font-size: 0.8rem;
+    font-weight: 400;
+    color: rgba(33, 33, 33, 1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 1rem;
 `;
 
-const InvoiceDashboard = () => {
-	return (
-		<InvoiceDashboardStyle>
-			<BannerImgID>
-				<img src={images.bgData2} alt=""></img>
-				<p>Hóa đơn</p>
-			</BannerImgID>
-      <BodyStylesID>
-      <Container fluid="md">
-          <Row className="justify-content-md-center">
-            <Col lg="11">
-              <TextStyleID>
-                <h3>Thời gian truy cập dữ liệu</h3>
-                <span>
-                  <div>
-                    <p>Thời gian bắt đầu</p>
-                    <input type="date"></input>
-                  </div>
-                  <div>
-                    <p>Thời gian kết thúc</p>
-                    <input type="date"></input>
-                  </div>
-                </span>
-              </TextStyleID>
-              
-              <TextStyleID>
-                <h3>Khu vực truy cập dữ liệu</h3>
-                <form>
-                  <select class="w3-select" name="option">
-                  <option value="" disabled selected>Địa phương</option>
-                    <option value="1">Cần Thơ</option>
-                    <option value="2">Đà Nẵng</option>
-                    <option value="3">Nha Trang</option>
-                    <option value="4">Thành phố Hồ Chí Minh</option>
-                  </select>
-                </form>
-              <content>
-                <form>
-                  <select class="w3-select" name="option">
-                  <option value="" disabled selected>Khu vực</option>
-                    <option value="1">TẤT CẢ</option>
-                    <option value="2">Khu vực Bắc</option>
-                    <option value="3">Khu vực Nam</option>
-                  </select>
-                </form>
-                <form>
-                  <select class="w3-select" name="option">
-                  <option value="" disabled selected>Trạm</option>
-                    <option value="1">TẤT CẢ</option>
-                    <option value="2">Trạm 1</option>
-                    <option value="3">Trạm 2</option>
-                    <option value="4">Trạm 3</option>
-                  </select>
-                </form>
-              </content>
-              </TextStyleID>
-              <BorderStyleID></BorderStyleID>
-              <Row className="justify-content-end">
-                <Col xs lg="2">
-                  <ButtonStyle variant="danger"><a href="#">Truy cập</a></ButtonStyle>{' '}
-                </Col>
-              </Row> 
-              
-            </Col>
-          </Row>
-        </Container>
-      </BodyStylesID>
-      <FooterStyle>
-        <p>
-          Bản quyền © 2023 thuộc về Trường Đại Học Kiến Trúc Thành phố Hồ Chí
-          Minh
-        </p>
-      </FooterStyle>
-		</InvoiceDashboardStyle>
-	);
+const ConsumeDashboard = () => {
+    const diaPhuongRef = useRef(null);
+    const khuVucRef = useRef(null);
+    const tramRef = useRef(null);
+    const timeStartRef = useRef(null);
+    const timeEndRef = useRef(null);
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [showError, setShowError] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = {
+            timeStart: timeStartRef.current.value,
+            timeEnd: timeEndRef.current.value,
+            diaPhuong: diaPhuongRef.current.value,
+            khuVuc: khuVucRef.current.value,
+            tram: tramRef.current.value,
+        };
+        console.log(data);
+    };
+    return (
+        <ConsumeDashboardStyle>
+            <BannerImgID>
+                <img src={images.bgData2} alt=""></img>
+                <p>Tiêu thụ</p>
+            </BannerImgID>
+            <Container fluid="md" className="mt-5">
+                <Row>
+                    <Col>
+                        <Form onSubmit={handleSubmit}>
+                            <TitleStyled>Thời gian truy cập</TitleStyled>
+                            <Row>
+                                <Col md={2}>
+                                    <b>Thời gian bắt đầu</b>
+                                    <Form.Control
+                                        type="date"
+                                        className="mt-2"
+                                        ref={timeStartRef}
+                                    ></Form.Control>
+                                </Col>
+                                <Col md={{ span: 2, offset: 5 }}>
+                                    <b>Thời gian kết thúc</b>
+                                    <Form.Control
+                                        type="date"
+                                        className="mt-2"
+                                        ref={timeEndRef}
+                                    ></Form.Control>
+                                </Col>
+                            </Row>
+                            <TitleStyled className="mt-5">
+                                Khu vực truy cập
+                            </TitleStyled>
+                            <Row className="justify-content-md">
+                                <Col xs lg="5">
+                                    <Form.Select
+                                        aria-label="Default select example"
+                                        ref={diaPhuongRef}
+                                    >
+                                        <option>Địa phương</option>
+                                        <option value="1">Cần Thơ</option>
+                                        <option value="2">Đà Nẵng</option>
+                                        <option value="3">Nha Trang</option>
+                                        <option value="3">
+                                            Thành phố Hồ Chí Minh
+                                        </option>
+                                    </Form.Select>
+                                </Col>
+                            </Row>
+                            <Row className="mt-5">
+                                <Col md={5}>
+                                    <Form.Select
+                                        aria-label="Default select example"
+                                        ref={khuVucRef}
+                                    >
+                                        <option>Khu vực</option>
+                                        <option value="1">TẤT CẢ</option>
+                                        <option value="2">Khu vực Bắc</option>
+                                        <option value="3">Khu vực Nam</option>
+                                    </Form.Select>
+                                </Col>
+                                <Col md={{ span: 5, offset: 2 }}>
+                                    <Form.Select
+                                        aria-label="Default select example"
+                                        ref={tramRef}
+                                    >
+                                        <option>Trạm</option>
+                                        <option value="1">TẤT CẢ</option>
+                                        <option value="2">Trạm 1</option>
+                                        <option value="3">Trạm 2</option>
+                                        <option value="4">Trạm 3</option>
+                                        <option value="5">Trạm 4</option>
+                                    </Form.Select>
+                                </Col>
+                            </Row>
+                            <Row className="justify-content-end border-top mt-5">
+                                <Col md={2}>
+                                    <ButtonStyle
+                                        variant="danger"
+                                        type="submit"
+                                        className="mt-5"
+                                    >
+                                        Truy cập
+                                    </ButtonStyle>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+            <FooterStyle>
+                <p>
+                    Bản quyền © 2023 thuộc về Trường Đại Học Kiến Trúc Thành phố
+                    Hồ Chí Minh
+                </p>
+            </FooterStyle>
+        </ConsumeDashboardStyle>
+    );
 };
 
-export default InvoiceDashboard;
+export default ConsumeDashboard;
