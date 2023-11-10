0bd427a9ff846b0d6ff040c17cf05e43f4682e57
Nguyễn Thành Tín <88455780+TomTom2k@users.noreply.github.com>
2023-11-11 05:23:59 +0700

    add table when upload file

--- src/pages/UpdateDataNew/UpdateDataConsume.js
+++ src/pages/UpdateDataNew/UpdateDataConsume.js
@@ -7,9 +7,35 @@ import AlterCus from '../../components/AlterCus';
 import LoadingCus from '../../components/LoadingCus';
 import ConfirmDialog from '../../components/ConfirmDialog';
 
+const renderTable = (sheet) => {
+	const cols = Object.keys(sheet);
+
+	return (
+		<Table striped bordered hover size="sm" responsive>
+			<thead>
+				<tr>
+					{cols.map((col, index) => (
+						<th key={index}>{col}</th>
+					))}
+				</tr>
+			</thead>
+			<tbody>
+				{Array.from({ length: 10 }).map((_, rowIndex) => (
+					<tr key={rowIndex}>
+						{cols.map((col, colIndex) => (
+							<td key={colIndex}>{sheet[col][rowIndex]}</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+		</Table>
+	);
+};
+
 const UpdateDataConsume = () => {
 	const [selectedFile, setSelectedFile] = useState(null);
 	const [data, setData] = useState(null);
+	const [sheet, setSheet] = useState(0);
 	const [isSuccess, setIsSuccess] = useState(false);
 	const [error, setError] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
@@ -40,11 +66,10 @@ const UpdateDataConsume = () => {
 		try {
 			const response = await fileApi.uploadTieuThu(selectedFile);
 			if ((response.status = 201)) {
-				setData(response.data);
+				setData(response.data.data);
 				setIsSuccess(true);
 			} else {
 				showAlter(setError, 'Tải file không thành công');
-				console.log('error: ', response.data);
 			}
 		} catch (error) {
 			console.error('Error uploading file:', error);
@@ -57,16 +82,17 @@ const UpdateDataConsume = () => {
 	};
 
 	const handleConfirm = async () => {
+		setIsLoading(true);
 		try {
 			const res = await fileApi.confirmTieuThu();
-			if ((res.status = 200)) {
-				setIsSuccess(false);
-			} else {
+			if (res.status !== 200) {
 				showAlter(setError, 'Lưu dữ liệu không thành công');
 			}
 		} catch (error) {
 			console.error('Error saving data:', error);
 		}
+		setIsSuccess(false);
+		setIsLoading(false);
 	};
 	return (
 		<>
@@ -127,6 +153,25 @@ const UpdateDataConsume = () => {
 						</div>
 					</Col>
 				</Row>
+				{data && (
+					<>
+						<Row className="mt-2">
+							<Col md={4}>
+								<Form.Select
+									aria-label="Default select example"
+									onChange={(e) => setSheet(e.target.value)}
+								>
+									{data.map((d, index) => (
+										<option value={index} key={index}>
+											sheet {index + 1}
+										</option>
+									))}
+								</Form.Select>
+							</Col>
+						</Row>
+						<Row className="mt-2">{renderTable(data[sheet])}</Row>
+					</>
+				)}
 			</Container>
 			<AlterCus show={!!error} variant="danger">
 				{error}
