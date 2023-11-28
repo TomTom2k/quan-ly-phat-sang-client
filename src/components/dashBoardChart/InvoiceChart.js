f0080bf321ff5fdf7a13e6c0f69b3a85cfe04896
Nguyễn Thành Tín <88455780+TomTom2k@users.noreply.github.com>
2023-11-28 22:00:53 +0700

    export data

--- src/components/dashBoardChart/InvoiceChart.js
+++ src/components/dashBoardChart/InvoiceChart.js
@@ -1,22 +1,70 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import {
 	Bar,
 	CartesianGrid,
 	ComposedChart,
-	Legend,
 	Tooltip,
 	XAxis,
 	YAxis,
 } from 'recharts';
+import * as XLSX from 'xlsx';
 
 import ConvertToTable from '../ConvertToTable';
 
 const InvoiceChart = ({ data, describe }) => {
+	const [isShow, setIsShow] = useState(true);
+
+	const exportToExcel = () => {
+		// Đổi tên các cột
+		const renamedData = data.map((item) => ({
+			Năm: item.year,
+			Tháng: item.month,
+			'Tổng Thành Tiền': item.tongThanhTien,
+		}));
+
+		// Chuyển đổi dữ liệu thành mảng 2D
+		const dataArray = renamedData.map((item) => Object.values(item));
+
+		// Tạo worksheet từ mảng dữ liệu
+		const ws = XLSX.utils.aoa_to_sheet([
+			Object.keys(renamedData[0]),
+			...dataArray,
+		]);
+
+		// Tạo workbook và thêm worksheet vào đó
+		const wb = XLSX.utils.book_new();
+		XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+		// Tạo tệp Excel và lưu nó vào ổ đĩa
+		XLSX.writeFile(wb, 'Dữ liệu hóa đơn.xlsx', {
+			bookType: 'xlsx',
+			bookSST: false,
+			type: 'binary',
+		});
+	};
+
 	return (
-		<Container>
+		<Container className="mt-5">
+			<Row className="justify-content-end g-5">
+				<Col md={2}>
+					<Row>
+						<Button onClick={() => setIsShow(!isShow)}>
+							{!isShow ? 'Hiện mô tả' : 'Ẩn mô tả'}
+						</Button>
+					</Row>
+				</Col>
+				<Col md={2}>
+					<Row>
+						<Button onClick={exportToExcel}>Xuất file</Button>
+					</Row>
+				</Col>
+			</Row>
+			<Row>
+				<h4 className="text-center">Khai thác dữ liệu hóa đơn</h4>
+			</Row>
 			<Row className="align-items-center justify-content-between gx-5">
-				<Col md={8}>
+				<Col md={isShow ? 8 : 12}>
 					<Row className="overflow-x-auto user-select-none  justify-content-center">
 						<ComposedChart
 							width={
